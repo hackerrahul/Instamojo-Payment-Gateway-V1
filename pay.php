a848d9b54d829974de8780f90f53cbc2556d13a2
HackerRahul <rahul13gangotri@gmail.com>
2017-10-09 23:00:06 +0530

    Initial commit

---
+++ pay.php
@@ -0,0 +1,40 @@
+<?php 
+$product_name = $_POST["product_name"];
+$price = $_POST["product_price"];
+$name = $_POST["name"];
+$phone = $_POST["phone"];
+$email = $_POST["email"];
+
+
+include 'src/instamojo.php';
+
+$api = new Instamojo\Instamojo('YOU_PRIVATE_API_KEY', 'YOUR_PRIVATE_AUTH_TOKEN','https://test.instamojo.com/api/1.1/');
+
+
+try {
+    $response = $api->paymentRequestCreate(array(
+        "purpose" => $product_name,
+        "amount" => $price,
+        "buyer_name" => $name,
+        "phone" => $phone,
+        "send_email" => true,
+        "send_sms" => true,
+        "email" => $email,
+        'allow_repeated_payments' => false,
+        "redirect_url" => "http://YOUR_WEBSITE.COM/thankyou.php",
+        "webhook" => "http://YOUR_WEBSITE.COM/webhook.php"
+        ));
+    //print_r($response);
+
+    $pay_ulr = $response['longurl'];
+    
+    //Redirect($response['longurl'],302); //Go to Payment page
+
+    header("Location: $pay_ulr");
+    exit();
+
+}
+catch (Exception $e) {
+    print('Error: ' . $e->getMessage());
+}     
+  ?>
